84f5f5bca4c73c18a828c83499bca55c3c8d0596
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2022-01-19 12:19:39 -0800

    Bug fix.

--- Scripts/Preliminary_Analysis.R
+++ Scripts/Preliminary_Analysis.R
@@ -26,90 +26,108 @@ phylogeny <- drop.tip(phylogeny, c(which(phylogeny$tip.label %ni% data$Species))
 data <- data[match(phylogeny$tip.label, data$Species),]
 
 #Generate the phylogenetically independent contrasts
-for(i in 7:ncol(data)){
-  DATA <- data[,i]
-  BAD <- which(is.na(DATA))
-  GOOD <- which(!is.na(DATA))
-  DATA <- DATA[GOOD]
+for(i in 7:ncol(data)){  #22 struggles with the seq(by = X) interval
   
-  if(length(DATA)>0){
-    if(is.numeric(data[,i])){
-      #Colour by trait value but some extra work to handle NA values
-      ncolors <- length(unique(DATA))
-      COLS <- viridis::viridis(ncolors)
-      my_df <- data.frame(trait=DATA, color=COLS[cut(DATA,ncolors)])
-      my_df_2 <- data.frame(trait=data[,i], species = data$Species)
-      df <- merge(my_df_2, my_df, by = "trait", all = T)
-      df <- df[!duplicated(df),]
-      df$color[is.na(df$color)] <- "black"
-      
-      #Generate the plot
-      path <- paste("Figures/", names(data)[i],".png", sep = "")
-      png(filename=path,
-          width = 6, height = 10, units = "in",
-          res = 300)
-      
-      plot(phylogeny,
-           tip.color=df$color,
-           cex = 0.42)
-      title(names(data)[i])
-      
-      legend_image <- as.raster(matrix(rev(COLS), ncol=1))
-      
-      par(mar=c(4, 2, 10, 12),                                  ## set margins
-          new=TRUE)
-      plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
-      lbsq <- seq.int(0, 1, l=5)
-      axis(4, at=lbsq, pos=0.1, labels=F, col=0, col.ticks=1, tck=-.05)
-      
-      
-      
-      text(x=0.4, y = seq(0,1,l=5), labels = seq(min(DATA),
-                                                 max(DATA),l=5),
-           cex = 0.75)
-      rasterImage(legend_image, 0, 0, 0.1,1)
-      
-      dev.off()
+  #Store the indexed trait
+  DATA <- data[,i]
+  
+  if(is.numeric(DATA)){
+    #Round
+    DATA <- round(DATA,2)
+    
+    #Min and max values
+    MIN <- min(na.omit(DATA))
+    MAX <- max(na.omit(DATA))
+    
+    #Sequence of numbers that spans the range of the data
+    VALS <- seq(MIN,
+                MAX,
+                by = 0.01)
+    
+    #Colours that span the range of the data
+    COL_VEC <- viridis::viridis(length(VALS))
+    
+    #Match colours with the data (NAs are grey)
+    COLS <- vector("character", length(DATA))
+    for(j in 1:length(DATA)){
+      KEEPER <- which(DATA[j] == VALS)
+      if(length(KEEPER)>0){COLS[j] <- COL_VEC[KEEPER]} else {COLS[j] <- "grey80"}
     }
     
-    if(is.character(data[,i])){
-      
-      DATA <- as.factor(DATA)
-      FAKE <- as.numeric(DATA) #Just for arranging the colours easily
-      
-      #Colour by trait value but some extra work to handle NA values
-      ncolors <- length(unique(DATA))
-      COLS <- viridis::viridis(ncolors)
-      my_df <- data.frame(trait=DATA, color=COLS[cut(FAKE,ncolors)])
-      my_df_2 <- data.frame(trait=data[,i], species = data$Species)
-      df <- merge(my_df_2, my_df, by = "trait", all = T)
-      df <- df[!duplicated(df),]
-      df$color[is.na(df$color)] <- "black"
-      
-      path <- paste("Figures/", names(data)[i],".png", sep = "")
-      png(filename=path,
-          width = 6, height = 10, units = "in",
-          res = 300)
-      
-      plot(phylogeny,
-           tip.color=df$color,
-           cex = 0.42)
-      title(names(data)[i])
-      
-      legend_image <- as.raster(matrix(rev(COLS), ncol=1))
-      
-      par(mar=c(4, 2, 10, 12),                                  ## set margins
-          new=TRUE)
-      plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
-      lbsq <- seq.int(0, 1, l=ncolors)
-      axis(4, at=lbsq, pos=0.1, labels=F, col=0, col.ticks=1, tck=-.05)
-      text(x=0.4, y = seq(0,1,l=ncolors), labels = levels(DATA),cex = 0.75)
-      rasterImage(legend_image, 0, 0, 0.1,1)
-      
-      dev.off()
+    #Generate the plot
+    path <- paste("Figures/", names(data)[i],".png", sep = "")
+    png(filename=path,
+        width = 6, height = 10, units = "in",
+        res = 300)
+    
+    plot(phylogeny,
+         tip.color=COLS,
+         cex = 0.42)
+    title(names(data)[i])
+    
+    legend_image <- as.raster(matrix(rev(COL_VEC), ncol=1))
+    
+    par(mar=c(4, 2, 10, 12),                                  ## set margins
+        new=TRUE)
+    plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
+    lbsq <- seq.int(0, 1, l=5)
+    axis(4, at=lbsq, pos=0.1, labels=F, col=0, col.ticks=1, tck=-.05)
+    
+    
+    
+    text(x=0.4, y = seq(0,1,l=5), labels = seq(MIN,MAX,l=5),
+         cex = 0.75)
+    rasterImage(legend_image, 0, 0, 0.1,1)
+    
+    dev.off()
+  }
+  
+  
+  if(is.character(DATA)){
+    
+    DATA <- as.factor(DATA)
+    FAKE <- as.numeric(DATA) #Just for arranging the colours easily
+    
+    
+    #Sequence of numbers that spans the range of the data
+    VALS <- unique(DATA)
+    
+    #Colours that span the range of the data
+    COL_VEC <- viridis::viridis(length(VALS))
+    
+    #Match colours with the data (NAs are grey)
+    COLS <- vector("character", length(DATA))
+    for(j in 1:length(DATA)){
+      KEEPER <- which(DATA[j] == VALS)
+      if(length(KEEPER)>0){COLS[j] <- COL_VEC[KEEPER]} else {COLS[j] <- "grey80"}
     }
     
-  } #Closes the if statement for the length of data > 0
+    #Colour by trait value but some extra work to handle NA values
+    ncolors <- length(unique(DATA))
+    
+    path <- paste("Figures/", names(data)[i],".png", sep = "")
+    png(filename=path,
+        width = 6, height = 10, units = "in",
+        res = 300)
+    
+    plot(phylogeny,
+         tip.color=COLS,
+         cex = 0.42)
+    title(names(data)[i])
+    
+    legend_image <- as.raster(matrix(rev(COL_VEC), ncol=1))
+    
+    par(mar=c(4, 2, 10, 12),                                  ## set margins
+        new=TRUE)
+    plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
+    lbsq <- seq.int(0, 1, l=ncolors)
+    axis(4, at=lbsq, pos=0.1, labels=F, col=0, col.ticks=1, tck=-.05)
+    text(x=0.4, y = seq(0,1,l=ncolors), labels = unique(DATA),cex = 0.75)
+    rasterImage(legend_image, 0, 0, 0.1,1)
+    
+    dev.off()
+  }
+  
 }
 
 
