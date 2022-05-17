11df403be6dce131a03ea5fa99aab09dc1cb6210
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2022-05-16 19:19:27 -0700

    minor tweak

--- Scripts/Cluster_Analysis.R
+++ Scripts/Cluster_Analysis.R
@@ -127,3 +127,78 @@ dev.off()
 
 
 
+
+#Run it again with only female traits
+data <- read.csv("Data/female_traits.csv")
+
+#Remove duplicate data on WTD "White-tailed deer (South America)"
+data <- data[-36,]
+
+#Select only the numeric columns
+data2 <- data[,unlist(lapply(data, is.numeric))]
+
+row.names(data2) <- data$Species
+dist <- daisy(data2,metric = "gower")
+hc <- hclust(dist)
+dend <- as.dendrogram(hc)
+
+plot(dend,
+     cex = 0.6)
+
+
+
+png(filename="Figures/Cluster_Female_Traits.png",
+    width = 6.86, height = 5, units = "in",
+    res = 600)
+#Label colours
+data3 <- data[match(labels(dend), data$Species),]
+LABS <- as.numeric(as.factor(data3$Family))
+COLS <- pals::ocean.curl(length(unique(LABS)))[LABS]
+labels_colors(dend) <- COLS
+labels_cex(dend) <-0.3
+#Generate plot
+plot(dend,
+     cex = 0.2,
+     axes=F,
+     main = "Coloured by Family")
+dev.off()
+
+
+
+
+#Run it again with only female traits
+data <- read.csv("Data/Male_traits.csv")
+
+#Remove duplicate data on WTD "White-tailed deer (South America)"
+data <- data[-36,]
+
+#Select only the numeric columns
+data2 <- data[,unlist(lapply(data, is.numeric))]
+
+row.names(data2) <- data$Species
+dist <- daisy(data2,metric = "gower")
+dist[is.na(dist)] <- 0
+hc <- hclust(dist)
+dend <- as.dendrogram(hc)
+
+plot(dend,
+     cex = 0.6)
+
+
+
+png(filename="Figures/Cluster_Male_Traits.png",
+    width = 6.86, height = 5, units = "in",
+    res = 600)
+#Label colours
+data3 <- data[match(labels(dend), data$Species),]
+LABS <- as.numeric(as.factor(data3$Family))
+COLS <- pals::ocean.curl(length(unique(LABS)))[LABS]
+labels_colors(dend) <- COLS
+labels_cex(dend) <-0.3
+#Generate plot
+plot(dend,
+     cex = 0.2,
+     axes=F,
+     main = "Coloured by Family")
+dev.off()
+
